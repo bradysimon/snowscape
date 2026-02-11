50ebf76c735216b9503ff70854425d39a9656e87
BradySimon <bradytsimon@gmail.com>
2026-02-10 22:57:20 -0500

    Add several new icons

--- src/icon.rs
+++ src/icon.rs
@@ -1,5 +1,19 @@
 use iced::widget::{Svg, svg};
 
+pub fn checkmark<'a>() -> Svg<'a> {
+    const BYTES: &[u8] =
+        include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/checkmark.svg"));
+    svg(svg::Handle::from_memory(BYTES))
+}
+
+pub fn circle_slash<'a>() -> Svg<'a> {
+    const BYTES: &[u8] = include_bytes!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/assets/circle-slash.svg"
+    ));
+    svg(svg::Handle::from_memory(BYTES))
+}
+
 pub fn refresh<'a>() -> Svg<'a> {
     const BYTES: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/refresh.svg"));
     svg(svg::Handle::from_memory(BYTES))
@@ -7,5 +21,10 @@ pub fn refresh<'a>() -> Svg<'a> {
 
 pub fn undo<'a>() -> Svg<'a> {
     const BYTES: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/undo.svg"));
+    svg(svg::Handle::from_memory(BYTES))
+}
+
+pub fn xmark<'a>() -> Svg<'a> {
+    const BYTES: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/xmark.svg"));
     svg(svg::Handle::from_memory(BYTES))
 }
